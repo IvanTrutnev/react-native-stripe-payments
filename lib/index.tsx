b2b7913b0ae123ae98aa143a0bd0219f95473af1
IvanTrutnev <trutnev_1996@mail.ru>
2021-03-17 11:13:04 +0300

    Add method to connect with account

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,34 +1,42 @@
-import { NativeModules } from 'react-native';
+import { NativeModules } from "react-native";
 
 const { StripePayments } = NativeModules;
 
 export interface InitParams {
-  publishingKey: string
+  publishingKey: string;
 }
 
 export interface CardDetails {
-  number: string,
-  expMonth: number,
-  expYear: number,
-  cvc: string,
+  number: string;
+  expMonth: number;
+  expYear: number;
+  cvc: string;
 }
 
 export interface PaymentResult {
-  id: string,
-  paymentMethodId: string,
+  id: string;
+  paymentMethodId: string;
 }
 
 class Stripe {
-  _stripeInitialized = false
+  _stripeInitialized = false;
 
   setOptions = (options: InitParams) => {
-    if (this._stripeInitialized) { return; }
+    if (this._stripeInitialized) {
+      return;
+    }
     StripePayments.init(options.publishingKey);
     this._stripeInitialized = true;
-  }
+  };
 
-  confirmPayment(clientSecret: string, cardDetails: CardDetails): Promise<PaymentResult> {
-    return StripePayments.confirmPayment(clientSecret, cardDetails)
+  setStripeAccount = (stripeAccount: string) =>
+    StripePayments.setStripeAccount(stripeAccount);
+
+  confirmPayment(
+    clientSecret: string,
+    cardDetails: CardDetails
+  ): Promise<PaymentResult> {
+    return StripePayments.confirmPayment(clientSecret, cardDetails);
   }
 
   isCardValid(cardDetails: CardDetails): boolean {
